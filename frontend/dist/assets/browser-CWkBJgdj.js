deac49be4152446a23066bd369a34efe54b2b2ce
Wersplat <wersplat@gmail.com>
2025-04-26 01:08:12 -0400

    🚀 New frontend build for deploy

---
+++ frontend/dist/assets/browser-CWkBJgdj.js
@@ -0,0 +1,2 @@
+import{a as o}from"./vendor-DrD-X_hS.js";var r,e;function t(){return e||(e=1,r=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}),r}var s=t();const n=o(s),a=Object.freeze(Object.defineProperty({__proto__:null,default:n},Symbol.toStringTag,{value:"Module"}));export{a as b};
+//# sourceMappingURL=browser-CWkBJgdj.js.map
