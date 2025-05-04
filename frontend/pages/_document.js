ded6024c0a917c57fd23721fb66ba9c11f829365
Wersplat <wersplat@users.noreply.github.com>
2025-05-04 08:18:47 -0400

    feat: create custom Document component for Next.js application
    # Format: <type>(scope): short summary
    # Types: feat | fix | docs | chore | refactor | perf | test | style

---
+++ frontend/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Add any additional meta tags or links here */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
